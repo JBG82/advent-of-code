6eee3b130c2c35c85a90bc66cd3f69c78132be07
JBG82 <jocheng@gmx.net>
2023-12-05 13:20:30 +0100

    Puzzle for day 5 2023 added.

---
+++ src/main/java/de/geburtig/advent/y2023/Day5.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) CredaRate Solutions GmbH
+ */
+package de.geburtig.advent.y2023;
+
+import de.geburtig.advent.util.InputResolver;
+
+import java.util.List;
+
+/**
+ * https://adventofcode.com/2023/day/5
+ */
+public class Day5 {
+    public static void main(final String[] args) {
+        List<String> input = InputResolver.fetchLinesFromInputFile("input_day5.txt", Day5.class);
+    }
+}
