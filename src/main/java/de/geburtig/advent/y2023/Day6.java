b691a01aee988f2b3289cb80c9a8f0e9793dd7a9
JBG82 <jocheng@gmx.net>
2023-12-06 17:20:09 +0100

    Puzzle for day 6 2023 added.

--- src/main/java/de/geburtig/advent/y2023/Day6.java
+++ src/main/java/de/geburtig/advent/y2023/Day6.java
@@ -5,6 +5,7 @@ package de.geburtig.advent.y2023;
 
 import de.geburtig.advent.util.InputResolver;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,5 +14,8 @@ import java.util.List;
 public class Day6 {
     public static void main(final String[] args) {
         List<String> input = InputResolver.fetchLinesFromInputFile("input_day6.txt", Day6.class);
+
+        List<Integer> times = Arrays.stream(input.getFirst().split("[ ]+")).filter(s -> s.matches("\\d+")).map(Integer::parseInt).toList();
+        times.forEach(System.out::println);
     }
 }
