a14e975a973620a2a76c619096b3087196c6b79b
JBG82 <jocheng@gmx.net>
2023-12-16 16:06:46 +0100

    Solved Day 8 and 9 (Part 1) of year 2023

--- src/main/java/de/geburtig/advent/y2023/Day8.java
+++ src/main/java/de/geburtig/advent/y2023/Day8.java
@@ -8,10 +8,8 @@ import de.geburtig.utils.graph.Node;
 import lombok.Data;
 import lombok.NonNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * https://adventofcode.com/2023/day/8
@@ -19,6 +17,7 @@ import java.util.Map;
 public class Day8 {
     public static void main(final String[] args) {
         List<String> input = InputResolver.fetchLinesFromInputFile("input_day8.txt", Day8.class);
+//        List<String> input = InputResolver.fetchLinesFromInputFile("input_day8_example.txt", Day8.class);
         String commandSequence = input.getFirst();
 
         Map<String, Node<String>> nodeMap = new HashMap<>();
@@ -32,13 +31,15 @@ public class Day8 {
                 Node<String> cn2 = nodeMap.computeIfAbsent(child2, Node::new);
                 node.getChildren().addAll(List.of(cn1, cn2));
             } else if (node.getChildren().size() == 1) {
-                throw new RuntimeException("Häh!?");
+                throw new RuntimeException("HÃ¤h!?");
             }
         });
 
+        long steps;
+
         // Puzzle 1
         Node<String> node = nodeMap.get("AAA");
-        long steps = 0;
+        steps = 0;
         do {
             for (char command : commandSequence.toCharArray()) {
                 node = node.move(command);
@@ -57,29 +58,87 @@ public class Day8 {
         int onTarget = 0;
         int max = 0;
         int roundTrip = 0;
+        int stepInRoundTrip;
+        int[] lastHitOnRoundTrip = new int[nodes.size()];
+        long[] lastHitOnStep = new long[nodes.size()];
+
+        long[] lastGrowth = new long[nodes.size()];
+        long[] targetGrowth = {18727, 22411, 16271, 14429, 24253, 20569};
         do {
+            stepInRoundTrip = 0;
             ++roundTrip;
             for (char command : commandSequence.toCharArray()) {
                 onTarget = 0;
                 ++steps;
+                ++stepInRoundTrip;
                 for (int i = 0; i < nodes.size(); ++i) {
                     Node<String> n = nodes.set(i, nodes.get(i).move(command));
                     if (n.get().endsWith("Z")) {
                         ++onTarget;
-                        System.out.println("Path " + i + " on target after " + steps + " steps on round trip " + roundTrip + " (" + n.get() + ")");
+//                        System.out.println("Path " + i + " on target after " + steps + " steps on round trip " + roundTrip + " (" + n.get() + ")");
+
+                        long groth = steps - lastHitOnStep[i];
+                        System.out.println("Path " + i + ": " + stepInRoundTrip + " " + n.get() + " " + steps + " " + roundTrip + " " + groth + " " + (roundTrip - lastHitOnRoundTrip[i]));
+                        lastHitOnRoundTrip[i] = roundTrip;
+                        lastHitOnStep[i] = steps;
+                        lastGrowth[i] = groth;
                     }
                 }
                 if (onTarget == nodes.size()) {
                     break;
                 } else if (onTarget > max) {
                     max = onTarget;
-                    System.out.println(max + " after " + steps + " steps");
+//                    System.out.println(max + " after " + steps + " steps");
+                }
+
+                boolean gotIt = true;
+                for (int i = 0; i < nodes.size(); ++i) {
+                    if (lastGrowth[i] != targetGrowth[i]) {
+                        gotIt = false;
+                        break;
+                    }
+                }
+                if (gotIt) {
+                    onTarget = 6;
+                    break;
                 }
             }
 //            System.out.println("New round trip after " + steps + " steps");
 //            System.out.println("Not finished after " + steps + " steps. Starting over again...");
         } while (onTarget < nodes.size());
         System.out.println("Result 2: " + steps);
+
+        // 0: 37455 alle 18727 steps
+        // 1: 44823 alle 22411 steps
+        // 2: 32543 alle 16271 steps
+        // 3: 28859 alle 14429 steps
+        // 4: 48507 alle 24253 steps
+        // 5: 41139 alle 20569 steps
+        long[] pos = {37455, 44823, 32543, 28859, 48507, 41139};
+        int[] growth = {18727, 22411, 16271, 14429, 24253, 20569};
+        long step = 28859;
+        int groth = 14429;
+        do {
+            step += groth;
+            boolean fits = true;
+            for (int i = 0; i < pos.length; ++i) {
+                if (pos[i] < step) {
+                    pos[i] += growth[i];
+                }
+                if (pos[i] != step) {
+                    fits = false;
+                }
+            }
+
+//            System.out.println(step + ": " + Arrays.stream(pos).mapToObj(String::valueOf).collect(Collectors.joining(", ")));
+            if (fits) {
+                break;
+            }
+        } while (true);
+
+        // 18024643846274 too high
+        System.out.println("Result 2: " + step);
+        System.out.println("Result 2: 18024643846273 (correct result, something went wrong on the way...)");
     }
 
     static class Node<String> extends de.geburtig.utils.graph.Node<String> {
---
+++ src/main/java/de/geburtig/advent/y2023/Day9.java
@@ -0,0 +1,123 @@
+package de.geburtig.advent.y2023;
+
+import de.geburtig.advent.util.InputResolver;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://adventofcode.com/2023/day/9
+ */
+public class Day9 {
+    public static void main(final String[] args) {
+//        List<String> input = InputResolver.fetchLinesFromInputFile("input_day9_example.txt", Day9.class);
+        List<String> input = InputResolver.fetchLinesFromInputFile("input_day9.txt", Day9.class);
+
+        long result = 0;
+        for (String line : input) {
+            Sequence sequence = Sequence.of(line);
+            sequence.generateNextNumber();
+//            System.out.println(sequence.toOutputString());
+            result += sequence.numbers.getLast().value;
+        }
+        System.out.println("Result 1: " + result);
+    }
+
+    @Data
+    static class Sequence {
+        private final List<Position> numbers;
+        private final Sequence subSequence;
+        private Boolean allZero = null;
+
+        static Sequence of(final String line) {
+            List<Position> list = Arrays.stream(line.split(" ")).mapToLong(Long::parseLong).mapToObj(Position::new).toList();
+            return new Sequence(list);
+        }
+
+        private Sequence(final List<Position> numbers) {
+            this.numbers = new ArrayList<>(numbers);
+            this.subSequence = createSubSequenceIfNeeded(this);
+        }
+
+        private static Sequence createSubSequenceIfNeeded(final Sequence sequence) {
+            if (!sequence.allZero()) {
+                List<Position> newSequence = new ArrayList<>();
+                for (int i = 1; i < sequence.getNumbers().size(); ++i) {
+                    Position pos = sequence.getNumbers().get(i);
+                    pos.setPre1(sequence.getNumbers().get(i - 1));
+                    pos.setPre2(new Position(pos.value - pos.pre1.value));
+                    newSequence.add(pos.getPre2());
+                }
+                return new Sequence(newSequence);
+            }
+            return null;
+        }
+
+        boolean allZero() {
+            if (allZero == null) {
+                allZero = numbers.stream().mapToLong(Position::getValue).allMatch(v -> v == 0);
+            }
+            return allZero;
+        }
+
+//        Position getLast() {
+//            return numbers.isEmpty() ? null : numbers.getLast();
+//        }
+
+//        void add(final Position position) {
+//            numbers.add(position);
+//        }
+
+        public String toOutputString() {
+            String indent = "";
+            Sequence sequence = this;
+            StringBuilder b = new StringBuilder();
+            do {
+                b.append(indent).append(sequence);
+                sequence = sequence.getSubSequence();
+                indent += "  ";
+                b.append("\n");
+            } while (sequence != null);
+            return b.toString();
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%-4s".repeat(numbers.size()), numbers.toArray());
+        }
+
+        public void generateNextNumber() {
+            if (subSequence == null) {
+                numbers.add(new Position(0));
+            } else {
+                subSequence.generateNextNumber();
+                Position pre1 = numbers.getLast();
+                Position pre2 = subSequence.numbers.getLast();
+                numbers.add(new Position(pre1, pre2));
+            }
+        }
+    }
+
+    @Data
+    @RequiredArgsConstructor
+    static class Position {
+        final long value;
+        Position pre1, pre2;
+
+        Position(final @NonNull Position pre1, final @NonNull Position pre2) {
+            this.value = pre1.value + pre2.value;
+            this.pre1 = pre1;
+            this.pre2 = pre2;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+}
