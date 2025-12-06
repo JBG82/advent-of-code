adabcf418c20d7052e353bb6716e15e994426e8e
Jochen Geburtig <jochen.geburtig@credarate.de>
2025-12-06 13:03:26 +0100

    Solved 10/2024 Part 2

--- src/main/java/de/geburtig/advent/y2024/Day10.java
+++ src/main/java/de/geburtig/advent/y2024/Day10.java
@@ -8,6 +8,7 @@ import de.geburtig.advent.util.Map2D;
 import de.geburtig.advent.util.Map2D.Direction;
 import de.geburtig.advent.util.Map2D.Field;
 import de.geburtig.advent.util.Map2D.Pos;
+import lombok.Data;
 
 import java.util.*;
 
@@ -20,7 +21,6 @@ public class Day10 {
 
     public static void main(String[] args) {
         Map2D<Integer> map = InputResolver.fetchInputAsIntMap("input_day10.txt");
-        map.print();
 
         List<Pos> trailheads = new ArrayList<>();
         List<Pos> peaks = new ArrayList<>();
@@ -30,41 +30,72 @@ public class Day10 {
         });
         System.out.println("Got " + trailheads.size() + " trailheads and " + peaks.size() + " peaks");
 
-        int result = 0;
+        ScanResult result = solve(trailheads, map);
+        // Part 1: 841
+        System.out.println("Part 1: " + result.reachedPeaks());
+        // Part 2: 1875
+        System.out.println("Part 2: " + result.distinctPathways());
+    }
+
+    private static ScanResult solve(List<Pos> trailheads, Map2D<Integer> map) {
+        int reachedPeaks = 0, distinctPaths = 0;
         for (Pos pos : trailheads) {
             Field<Integer> trailhead = map.getField(pos);
             Map2D<Boolean> beenThereMap = new Map2D<>(map.getWidth(), map.getHeight(), false);
-            Set<Field<Integer>> reachablePeaks = scanTrailways(trailhead, beenThereMap);
-//            System.out.println(reachablePeaks.size());
-            result += reachablePeaks.size();
+            ScanResult scanResult = scanTrailways(trailhead, beenThereMap);
+            reachedPeaks += scanResult.reachedPeaks();
+            distinctPaths += scanResult.distinctPathways;
         }
-
-        // Part 1: 841
-        System.out.println(result);
+        return new ScanResult(reachedPeaks, distinctPaths);
     }
 
-    static Set<Field<Integer>> scanTrailways(final Field<Integer> field, Map2D<Boolean> beenThereMap) {
-        Set<Field<Integer>> reachedPeaks = new HashSet<>();
-        wander(field, beenThereMap, reachedPeaks);
-        return reachedPeaks;
+    static ScanResult scanTrailways(final Field<Integer> field, Map2D<Boolean> beenThereMap) {
+        Set<Pos> reachedPeaks = new HashSet<>();
+        Set<Path> fullPaths = new HashSet<>();
+
+        Path path = new Path(field.getPos());
+        wander(field, beenThereMap, reachedPeaks, path, fullPaths);
+
+        return new ScanResult(reachedPeaks.size(), fullPaths.size());
     }
 
-    private static void wander(Field<Integer> field, Map2D<Boolean> beenThereMap, Set<Field<Integer>> reachedPeaks) {
+    private static void wander(Field<Integer> field, Map2D<Boolean> beenThereMap, Set<Pos> reachedPeaks, Path path, Set<Path> distinctPaths) {
         beenThereMap.set(field.getPos(), true);
         if (field.getValue() == 9) {
-            reachedPeaks.add(field);
+            reachedPeaks.add(field.getPos());
+            distinctPaths.add(path);
         } else {
-            tryAndGo(north, field, beenThereMap, reachedPeaks);
-            tryAndGo(east, field, beenThereMap, reachedPeaks);
-            tryAndGo(south, field, beenThereMap, reachedPeaks);
-            tryAndGo(west, field, beenThereMap, reachedPeaks);
+            tryAndGo(north, field, beenThereMap, reachedPeaks, path, distinctPaths);
+            tryAndGo(east, field, beenThereMap, reachedPeaks, path, distinctPaths);
+            tryAndGo(south, field, beenThereMap, reachedPeaks, path, distinctPaths);
+            tryAndGo(west, field, beenThereMap, reachedPeaks, path, distinctPaths);
         }
     }
 
-    private static void tryAndGo(final Direction direction, final Field<Integer> field, Map2D<Boolean> beenThereMap, Set<Field<Integer>> reachedPeaks) {
+    private static void tryAndGo(final Direction direction, final Field<Integer> field, Map2D<Boolean> beenThereMap, Set<Pos> reachedPeaks, Path path, Set<Path> distinctPaths) {
         Optional<Field<Integer>> adjacent = field.getAdjacent(direction);
-        if (adjacent.isPresent() && !beenThereMap.get(adjacent.get().getPos()) && adjacent.get().getValue() == field.getValue() + 1) {
-            wander(adjacent.get(), beenThereMap, reachedPeaks);
+        if (adjacent.isPresent() && adjacent.get().getValue() == field.getValue() + 1) {
+            wander(adjacent.get(), beenThereMap, reachedPeaks, path.next(adjacent.get().getPos()), distinctPaths);
+        }
+    }
+
+    record ScanResult(int reachedPeaks, int distinctPathways) {}
+
+    @Data
+    private static class Path {
+        private final Pos[] steps;
+
+        Path(Pos head) {
+            steps = new Pos[]{head};
+        }
+
+        Path(Path base, Pos tail) {
+            steps = Arrays.copyOf(base.steps, base.steps.length + 1);
+            steps[base.steps.length] = tail;
+        }
+
+        Path next(Pos pos) {
+            return new Path(this, pos);
         }
     }
 }
